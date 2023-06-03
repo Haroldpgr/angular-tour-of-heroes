059e146a60d23d03c085b2865a7caac54dbfd6bf
[Haroldpgr] <[haroladresamezquitanitola@gma.com]>
2023-06-02 21:21:43 -0500

    Tema de Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', poder : 'Control mental' },
+  { id: 13, name: 'Bombasto' , poder : 'Lanzar bombas'},
+  { id: 14, name: 'Celeritas' , poder : 'Acelerar super rapido'},
+  { id: 15, name: 'Magneta' , poder : 'Contro de los metales'},
+  { id: 16, name: 'RubberMan' , poder : 'Fuerza'},
+  { id: 17, name: 'Dynama' , poder : 'Indestrutible'},
+  { id: 18, name: 'Dr. IQ' , poder : 'Quimico'},
+  { id: 19, name: 'Magma' , poder : 'Poder de magma'},
+  { id: 20, name: 'Tornado' , poder : 'Tornado'}
+];
