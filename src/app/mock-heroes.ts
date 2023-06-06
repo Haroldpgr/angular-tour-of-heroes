ef93b88820872cb77bfeee024b240d89eb884492
[Haroldpgr] <[haroladresamezquitanitola@gma.com]>
2023-06-05 20:54:47 -0500

    Fin del proyecto

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,7 +4,7 @@ export const HEROES: Hero[] = [
   { id: 12, name: 'Dr. Nice', poder : 'Control mental' },
   { id: 13, name: 'Bombasto' , poder : 'Lanzar bombas'},
   { id: 14, name: 'Celeritas' , poder : 'Acelerar super rapido'},
-  { id: 15, name: 'Magneta' , poder : 'Contro de los metales'},
+  { id: 15, name: 'Magneta' , poder : 'Control de los metales'},
   { id: 16, name: 'RubberMan' , poder : 'Fuerza'},
   { id: 17, name: 'Dynama' , poder : 'Indestrutible'},
   { id: 18, name: 'Dr. IQ' , poder : 'Quimico'},
